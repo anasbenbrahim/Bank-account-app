8b8858b06c9ddceb9329b6f5172e02730cd56f61
anasbenbrahim <anasbenbrahim491@gmail.com>
2025-07-11 17:00:59 +0100

    feat: configure Gateway Service and Service Discovery with Eureka for dynamic routing

---
+++ account-service/src/main/java/me/project/accountservice/client/CustomerRestClient.java
@@ -0,0 +1,4 @@
+package me.project.accountservice.client;
+
+public interface CustomerRestClient {
+}
--- gateway-service/src/main/java/me/project/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/me/project/gatewayservice/GatewayServiceApplication.java
@@ -2,12 +2,24 @@ package me.project.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+
+	@Bean
+	DiscoveryClientRouteDefinitionLocator locator (
+			ReactiveDiscoveryClient rcd,
+			DiscoveryLocatorProperties dlp
+	){
+		return new DiscoveryClientRouteDefinitionLocator(rcd,dlp);
 	}
 
 }
